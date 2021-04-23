045ae65153b73727ace8bf0e307955863a2d2077
Jhonata Caio <jhonatacaiob@gmail.com>
2021-04-22 21:53:35 -0300

    Finalizado

--- forca.py
+++ forca.py
@@ -1,12 +1,22 @@
 import random, math
 
-def tem_caractere(um_caractere, uma_string):
-    if(uma_string.find(um_caractere) == -1):
+def tem_caractere(um_caractere, uma_serie_de_letras):
+    if(uma_serie_de_letras.count(um_caractere) == 0):
         return False
     else:
         return True
 
-def revelar_letra(uma_palavra, a_palavra_mostrada, uma_letra):
+def e_palavra(uma_palavra):
+    if(not tem_caractere(" ", uma_palavra) and palavra.isalpha()):
+        return True
+    else:
+        print("\nDigite apenas uma palavra, sem numeros")
+        print("+---------------------------------+")
+        return False
+
+
+
+def revelar_letras(uma_palavra, a_palavra_mostrada, uma_letra):
     
     uma_palavra_vetor = " ".join(uma_palavra).split(" ")
     a_palavra_mostrada_vetor = " ".join(a_palavra_mostrada).split(" ")
@@ -19,7 +29,7 @@ def revelar_letra(uma_palavra, a_palavra_mostrada, uma_letra):
     return "".join(a_palavra_mostrada_vetor)
 
 def revelar_uma_letra(uma_palavra, a_palavra_mostrada, um_indice):
-    print(um_indice)
+    
     uma_palavra_vetor = " ".join(uma_palavra).split(" ")
     a_palavra_mostrada_vetor = " ".join(a_palavra_mostrada).split(" ")
     
@@ -33,11 +43,9 @@ palavra = ""
 while True:
     #palavra = input("Digite uma palavra: ").lower()
     palavra = "banana"
-    if(not tem_caractere(" ", palavra) and palavra.isalpha()):
+    if(e_palavra(palavra)):
         break
-    else:
-        print("\nDigite apenas uma palavra, sem numeros")
-        print("+---------------------------------+")
+
 
 
 letras_chutadas = []
@@ -45,11 +53,13 @@ numero_de_vidas = 6
 numero_de_dicas = math.floor(len(palavra) / 3)
 palavra_mostrada = "_" * len(palavra)
 
+
 for i in range(random.randint(100,1000)):
     print()
 
 print("+----------JOGO DA FORCA----------+")
 while True:
+    
     print("+---------------------------------+")
     print(f"Você tem {numero_de_vidas} vidas e {numero_de_dicas} dicas")
     print("O que deseja fazer")
@@ -57,10 +67,17 @@ while True:
     print("2. Chutar uma letra")
     print("3. Chutar a palavra")
     print("0. Sair")
-    #opcao = int(input(": "))
-    opcao = 2
+    opcao = input(": ")
+    if(not opcao.isalpha()):
+        opcao = int(opcao)
+        if(opcao < 0 or opcao > 3):
+            print("######## Opcao invalida ###########")
+    else:
+        print("######## Digite apenas numero ###########")
+
+   
     if(opcao == 1): 
-        if(numero_de_dicas < 0):
+        if(numero_de_dicas <= 0):
             print("Sem mais dicas")
         else:
             indice_aleatorio = random.randint(0, len(palavra) - 1)
@@ -71,22 +88,22 @@ while True:
 
     if(opcao == 2):
         letra = input("Digite uma letra: ").lower()[0]
-        if(letras_chutadas.count(letra) == 0 and letra.isalpha() and palavra.count(letra) > 0):
-            
-            palavra_mostrada = revelar_letra(palavra, palavra_mostrada, letra)
-            letras_chutadas.append(letra)
-            print(">>>>>>>>>>>" + palavra_mostrada)
         
-        elif(not letra.isalpha()):
+        if(not letra.isalpha()):
             print("Digite apenas letras")
 
-        elif(letras_chutadas.count(letra) > 0):
-            print("Esta letra ja foi chutada")
+        elif(tem_caractere(letra, letras_chutadas)):
+            print("Esta letra ja foi chutada!! Tente outra")
 
-        elif(palavra.count(letra) == 0):
-            print("Não tem essa letra")
+        elif(not tem_caractere(letra, palavra)):
+            print("Não tem essa letra!! -1 vida")
             letras_chutadas.append(letra)
             numero_de_vidas -= 1
+
+        else:
+            palavra_mostrada = revelar_letras(palavra, palavra_mostrada, letra)
+            letras_chutadas.append(letra)
+            print(palavra_mostrada)
 
         if(numero_de_vidas == 0):
 
@@ -98,7 +115,15 @@ while True:
             exit()
 
     if(opcao == 3):
-        pass
+        print("Se você errar, você perde")
+        palavra_chutada = input("Digite uma palavra: ")
+        if(not tem_caractere(" ", palavra) and palavra.isalpha()):
+            if(palavra_chutada==palavra):
+                print("PARABENS!!! VOCÊ ACERTOU")
+            else:
+                print("Uma pena, você errou")
+            exit()
+        
         #chutar palavra
     if(opcao == 0):
-        break
+        exit()
